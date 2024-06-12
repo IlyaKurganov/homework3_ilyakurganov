8f115de2942041b602a94a00a993c0cfbff4123d
IlyaKurganov <134068215+IlyaKurganov@users.noreply.github.com>
2024-06-12 09:09:47 +0500

    Add files via upload

--- src/homework/lesson2/tv/App.java
+++ src/homework/lesson2/tv/App.java
@@ -2,14 +2,14 @@ package homework.lesson2.tv;
 
 public class App {
     public static void main(String[] args) {
+
         Televisor tv = new Televisor();
         tv.turnOn();
-        tv.fullCheck();
+        tv.setRandomChannel();
+        tv.setVolume(10);
 
-        Televisor tv2 = new Televisor(true, 5, 5);
-        tv2.fullCheck();
+        System.out.println(tv.toString());
 
-        Televisor tv3 = new Televisor(true, 6);
-        tv3.fullCheck();
+
     }
 }
